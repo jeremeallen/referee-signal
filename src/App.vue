329329fbc77ed2ad8d685a38b9ffce831dedafba
jeremeallen <jallen@allensservices.com>
2017-01-25 08:17:51 -0600

    Styling of all panel images

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app" class='container'>
-    <div class='columns is-full'>
-      <div class="tile is-ancestor">
-        <div class="tile is-parent box" v-for='penalty in penalties'>
-          <article class="tile is-child notification">
-            <p class="title">{{ penalty.name }}</p>
-            <p class="subtitle">With an image</p>
-            <figure class="image penalties" :class="penalty.cssClass">
-            </figure>
-          </article>
+  <div id="app" class='container-fluid'>
+    <div class='row'>
+      <div class="col-xs-12 col-sm-4 col-md-3" v-for='penalty in penalties'>
+        <div class="panel panel-default">
+          <div class="panel-heading">{{ penalty.name }}</div>
+          <div class="panel-body">
+            <div class="image penalties" :class="penalty.cssClass"></div>
+          </div>
         </div>
       </div>
     </div>
@@ -17,33 +15,12 @@
 
 <script>
 
+import penalties from './data';
+
 export default {
   name: 'app',
   data() {
-    return {
-      penalties: [
-        {
-          name: 'Boarding',
-          description: 'When you board someone',
-          cssClass: 'boarding',
-        },
-        {
-          name: 'Butt-ending',
-          description: 'When you butt end someone',
-          cssClass: 'butt-ending',
-        },
-        {
-          name: 'Charging',
-          description: 'When you do the charging',
-          cssClass: 'charging',
-        },
-        {
-          name: 'Contact to the head',
-          description: 'When you hit someone in the head',
-          cssClass: 'head-contact',
-        },
-      ],
-    };
+    return penalties;
   },
 };
 </script>
@@ -51,8 +28,9 @@ export default {
   .penalties {
     background-image: url('./assets/RefereePenaltysignals.png');
     background-repeat: no-repeat;
-    width: 250px;
-    height: 300px;
+    width: 210px;
+    height: 240px;
+    margin: 0 auto;
   }
 
   .boarding {
@@ -60,14 +38,86 @@ export default {
   }
 
   .butt-ending {
-    background-position: -220px -10px;
+    background-position: -240px -10px;
   }
 
   .charging {
-    background-position: -440px -10px;
+    background-position: -470px -10px;
   }
 
   .head-contact {
-    background-position: -660px -10px;
+    background-position: -720px -15px;
+  }
+
+  .cross-checking {
+    background-position: -940px -10px;
+  }
+
+  .delayed-call {
+    background-position: -1160px -10px;
+  }
+
+  .delayed-game {
+    background-position: -5px -320px;
+  }
+
+  .elbowing {
+    background-position: -220px -320px;
+  }
+
+  .roughing {
+    background-position: -480px -320px;
+  }
+
+  .hand-pass {
+    background-position: -710px -320px;
+  }
+
+  .high-sticking {
+    background-position: -940px -320px;
+  }
+
+  .hitting-behind {
+    background-position: -1160px -320px;
+  }
+
+  .holding {
+    background-position: -5px -630px;
+  }
+
+  .holding-stick {
+    background-position: -240px -630px;
+  }
+
+  .hooking {
+    background-position: -710px -630px;
+  }
+
+  .icing {
+    background-position: -940px -630px;
+  }
+
+  .unsportsmanlike {
+    background-position: -1160px -630px;
+  }
+
+  .tripping {
+    background-position: -110px -940px;
+  }
+
+  .kneeing {
+    background-position: -360px -940px;
+  }
+
+  .misconduct {
+    background-position: -580px -940px;
+  }
+
+  .spearing {
+    background-position: -810px -940px;
+  }
+
+  .slashing {
+    background-position: -1050px -940px;
   }
 </style>
---
+++ src/data.js
@@ -0,0 +1,119 @@
+export default {
+  penalties: [
+    {
+      name: 'Boarding',
+      description: 'When you board someone',
+      cssClass: 'boarding',
+    },
+    {
+      name: 'Butt-ending',
+      description: 'When you butt end someone',
+      cssClass: 'butt-ending',
+    },
+    {
+      name: 'Charging',
+      description: 'When you do the charging',
+      cssClass: 'charging',
+    },
+    {
+      name: 'Contact to the head',
+      description: 'When you hit someone in the head',
+      cssClass: 'head-contact',
+    },
+    {
+      name: 'Cross checking',
+      description: 'Cross checking',
+      cssClass: 'cross-checking',
+    },
+    {
+      name: 'Delayed calling of penalty',
+      description: 'Delayed call',
+      cssClass: 'delayed-call',
+    },
+    {
+      name: 'Delay of game',
+      description: 'Delayed of game',
+      cssClass: 'delayed-game',
+    },
+    {
+      name: 'Elbowing',
+      description: 'Elbowing',
+      cssClass: 'elbowing',
+    },
+    {
+      name: 'Roughing',
+      description: 'roughing',
+      cssClass: 'roughing',
+    },
+    {
+      name: 'Hand pass',
+      description: 'Hand pass',
+      cssClass: 'hand-pass',
+    },
+    {
+      name: 'High sticking',
+      description: 'High sticking',
+      cssClass: 'high-sticking',
+    },
+    {
+      name: 'Hitting from behind',
+      description: 'Hitting from behind',
+      cssClass: 'hitting-behind',
+    },
+    {
+      name: 'Holding',
+      description: 'Holding',
+      cssClass: 'holding',
+    },
+    {
+      name: 'Holding the stick',
+      description: 'Holding the stick',
+      cssClass: 'holding-stick',
+    },
+    {
+      name: 'Interference',
+      description: 'interference',
+      cssClass: 'interference',
+    },
+    {
+      name: 'Hooking',
+      description: 'hooking',
+      cssClass: 'hooking',
+    },
+    {
+      name: 'Icing',
+      description: 'icing',
+      cssClass: 'icing',
+    },
+    {
+      name: 'Unsportsmanlike conduct',
+      description: 'unsportsmanlike conduct',
+      cssClass: 'unsportsmanlike',
+    },
+    {
+      name: 'Tripping',
+      description: 'tripping',
+      cssClass: 'tripping',
+    },
+    {
+      name: 'Kneeing',
+      description: 'kneeing',
+      cssClass: 'kneeing',
+    },
+    {
+      name: 'Misconduct',
+      description: 'misconduct',
+      cssClass: 'misconduct',
+    },
+    {
+      name: 'Spearing',
+      description: 'spearing',
+      cssClass: 'spearing',
+    },
+    {
+      name: 'Slashing',
+      description: 'slashing',
+      cssClass: 'slashing',
+    },
+  ],
+};
